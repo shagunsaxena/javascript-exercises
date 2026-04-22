c8ce55831e488cc6188b539af1918e31fa1cea1b
Shagun Saxena <shagunsaxena96@gmail.com>
2026-04-23 00:01:39 +0530

    06_commit

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string,times) {
+    if(times < 0)return "ERROR";
+    let result = "";
+    for(let i = 0; i < times; i++){
+        result += string;
+    }
+    return result;
 
 };
 
